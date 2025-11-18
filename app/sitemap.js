cab2b0c866621597af1af7221acdeef8151f0731
tory woods <40467443+Young-tkz@users.noreply.github.com>
2025-11-18 17:16:16 +0530

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://tokmexxx.vercel.app/",
+      lastModified: new Date().toISOString(),
+      changeFrequency: "daily",
+      priority: 1.0,
+    },
+  ];
+}
