3ca694103612b35a94f5b773e80f602872c0ce96
tory woods <40467443+Young-tkz@users.noreply.github.com>
2025-11-18 16:59:10 +0530

    Add Google site verification to metadata

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,8 @@ export const metadata = {
     title: "TokMe - TikTok Video Downloader (No Watermark)",
     description:
         "Fast, free TikTok video downloader. Download TikTok videos in HD without watermark on mobile or desktop.",
+    other: {"google-site-verification": "sOKRH3WPEivr0fLAmVTuuFHy_dBBAub3R5A9nqPQcNo",
+           },
 };
 
 export default function RootLayout({ children }) {
